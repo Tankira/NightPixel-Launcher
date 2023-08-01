e54d246aa7460cc6a4c3764ccef54449c8fb5a02
Tankira <108786645+Tankira@users.noreply.github.com>
2023-08-01 07:32:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+$("a").on("click", function (e) {
+    // 1 
+    e.preventDefault();
+    // 2 
+    const href = $(this).attr("href");
+    // 3 
+    $("html, body").animate({ scrollTop: $(href).offset().top }, 800);
+});
